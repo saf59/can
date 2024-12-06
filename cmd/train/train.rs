48bad828151d42f961cbde6556f7b38dfbf40029
alsh <shpirkov@gmail.com>
2024-12-06 19:55:11 +0200

    add train

---
+++ cmd/train/train.rs
@@ -0,0 +1,146 @@
+#[cfg(feature = "accelerate")]
+extern crate accelerate_src; // This should reach 91.5% accuracy.
+#[cfg(feature = "mkl")]
+extern crate intel_mkl_src;
+
+use clap::{Parser, ValueEnum};
+
+use candle_core::{DType, Result, Tensor, D};
+use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap};
+
+const IMAGE_DIM: usize = 784;
+const LABELS: usize = 10;
+
+trait Model: Sized {
+    fn new(vs: VarBuilder) -> Result<Self>;
+    fn forward(&self, xs: &Tensor) -> Result<Tensor>;
+}
+
+struct Mlp {
+    ln1: Linear,
+    ln2: Linear,
+}
+
+impl Model for Mlp {
+    fn new(vs: VarBuilder) -> Result<Self> {
+        let ln1 = candle_nn::linear(IMAGE_DIM, 100, vs.pp("ln1"))?;
+        let ln2 = candle_nn::linear(100, LABELS, vs.pp("ln2"))?;
+        Ok(Self { ln1, ln2 })
+    }
+
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        let xs = self.ln1.forward(xs)?;
+        let xs = xs.relu()?;
+        self.ln2.forward(&xs)
+    }
+}
+
+struct TrainingArgs {
+    learning_rate: f64,
+    load: Option<String>,
+    save: Option<String>,
+    epochs: usize,
+}
+
+fn training_loop<M: Model>(
+    m: candle_datasets::vision::Dataset,
+    args: &TrainingArgs,
+) -> anyhow::Result<()> {
+    let dev = candle_core::Device::cuda_if_available(0)?;
+
+    let train_labels = m.train_labels;
+    let train_images = m.train_images.to_device(&dev)?;
+    let train_labels = train_labels.to_dtype(DType::U32)?.to_device(&dev)?;
+
+    let mut varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &dev);
+    let model = M::new(vs.clone())?;
+
+    if let Some(load) = &args.load {
+        println!("loading weights from {load}");
+        varmap.load(load)?
+    }
+
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), args.learning_rate)?;
+    let test_images = m.test_images.to_device(&dev)?;
+    let test_labels = m.test_labels.to_dtype(DType::U32)?.to_device(&dev)?;
+    for epoch in 1..args.epochs {
+        let logits = model.forward(&train_images)?;
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?;
+        let loss = loss::nll(&log_sm, &train_labels)?;
+        sgd.backward_step(&loss)?;
+
+        let test_logits = model.forward(&test_images)?;
+        let sum_ok = test_logits
+            .argmax(D::Minus1)?
+            .eq(&test_labels)?
+            .to_dtype(DType::F32)?
+            .sum_all()?
+            .to_scalar::<f32>()?;
+        let test_accuracy = sum_ok / test_labels.dims1()? as f32;
+        println!(
+            "{epoch:4} train loss: {:8.5} test acc: {:5.2}%",
+            loss.to_scalar::<f32>()?,
+            100. * test_accuracy
+        );
+    }
+    if let Some(save) = &args.save {
+        println!("saving trained weights in {save}");
+        varmap.save(save)?
+    }
+    Ok(())
+}
+
+#[derive(ValueEnum, Clone)]
+enum WhichModel {
+    Linear,
+    Mlp,
+    Cnn,
+}
+
+#[derive(Parser)]
+struct Args {
+    #[clap(value_enum, default_value_t = WhichModel::Linear)]
+    model: WhichModel,
+
+    #[arg(long)]
+    learning_rate: Option<f64>,
+
+    #[arg(long, default_value_t = 200)]
+    epochs: usize,
+
+    /// The file where to save the trained weights, in safetensors format.
+    #[arg(long)]
+    save: Option<String>,
+
+    /// The file where to load the trained weights from, in safetensors format.
+    #[arg(long)]
+    load: Option<String>,
+
+    /// The directory where to load the dataset from, in ubyte format.
+    #[arg(long)]
+    local_mnist: Option<String>,
+}
+
+pub fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    // Load the dataset
+    let m = if let Some(directory) = args.local_mnist {
+        candle_datasets::vision::mnist::load_dir(directory)?
+    } else {
+        candle_datasets::vision::mnist::load()?
+    };
+    println!("train-images: {:?}", m.train_images.shape());
+    println!("train-labels: {:?}", m.train_labels.shape());
+    println!("test-images: {:?}", m.test_images.shape());
+    println!("test-labels: {:?}", m.test_labels.shape());
+
+    let default_learning_rate = 0.05;
+    let training_args = TrainingArgs {
+        epochs: args.epochs,
+        learning_rate: args.learning_rate.unwrap_or(default_learning_rate),
+        load: args.load,
+        save: args.save,
+    };
+    training_loop::<Mlp>(m, &training_args)
+}
--- crates/can_data/can_data.rs
+++ crates/can_data/can_data.rs
@@ -1,5 +1,57 @@
 use candle_core::{Device, Tensor};
 
+pub struct Dataset {
+    pub train_data: Tensor,
+    pub train_labels: Tensor,
+    pub test_data: Tensor,
+    pub test_labels: Tensor,
+    pub labels: usize,
+    pub data_width: usize,
+}
+/*pub fn load_dir<T: AsRef<std::path::Path>>(dir: T) -> candle_core::Result<Dataset> {
+    let dir = dir.as_ref();
+    //let (x,y) = read_x_y(&dir)?;
+
+}
+*/
+fn read_medius_x(dir: &std::path::Path) -> candle_core::Result<(Vec<f32>)> {
+    let x_entry = std::fs::File::open(&dir.join("x.csv"))?;
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(x_entry);
+    let mut data = vec![0f32; 100];
+    for row in reader.records() {
+        let row = match row {
+            Err(_) => continue,
+            Ok(row) => {
+                if row.len() > 0 {
+                    row.iter() //.map(f32::from)
+                        .for_each(|v| data.push(v.parse().unwrap()))
+                }
+            }
+        };
+    }
+    Ok(data)
+}
+fn read_medius_y(dir: &std::path::Path) -> candle_core::Result<(Vec<u8>)> {
+    println!("Path: {:?}",&dir.join("y.csv"));
+    let x_entry = std::fs::File::open(&dir.join("y.csv"))?;
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(x_entry);
+    let mut data = vec![0u8];
+    for row in reader.records() {
+        let row = match row {
+            Err(_) => continue,
+            Ok(row) => {
+                if row.len()>0 { data.push(row.get(0).unwrap().parse().unwrap())}
+            }
+        };
+    }
+    Ok(data)
+}
+
+
 pub fn data_set(device: &Device) -> (Tensor, Tensor) {
     let x = Tensor::from_slice(
         &[
@@ -35,5 +87,12 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_x_y() {
+        //let y = read_medius_y("W:/data/medius/src/V3/stat_n260Tlist".as_ref()).unwrap();
+        let y = read_medius_y("../../data/stat_n260Tlist".as_ref()).unwrap();
+        println!("{:?},{:?},{:?}",y.len(),y[1],y.last())
     }
 }

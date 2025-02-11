4a07577cf405157356a6e7d8e243164cae4d85f5
alsh <shpirkov@gmail.com>
2025-02-11 20:14:24 +0200

    add bicks

--- cmd/bicks/bicks.rs
+++ cmd/bicks/bicks.rs
@@ -112,7 +112,7 @@ fn show(sampling_rate: f32, original_signal: &[f32]) {
     //plot.show();
     plot.write_html("bicks.html");
 }
-#[cfg(target_os = "linux")]
+#[cfg(target_os = "android")]
 fn show(sampling_rate: f32, original_signal: &Vec<f32>) {
     let freqs = [20.0, 30.0, 50.0];
     let x: Vec<f32> = (0..100).map(|x| x as f32).collect();

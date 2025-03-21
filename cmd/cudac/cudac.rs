f6e5e3826f236e76c1ad4d3dddf6e98f50949566
alsh <shpirkov@gmail.com>
2025-03-21 14:47:46 +0200

    rem cuda feature in cudac

--- cmd/cudac/cudac.rs
+++ cmd/cudac/cudac.rs
@@ -1,6 +1,8 @@
 fn main() {
+    // cuda.md with details about CUDA
 	#[cfg(feature = "cuda")]
     unsafe {
-        println!("CUDA available: {}", cuda_driver_sys::cuInit(0) == cuda_driver_sys::CUresult::CUDA_SUCCESS);
+        println!("CUDA   available: {}", cuda_driver_sys::cuInit(0) == cuda_driver_sys::CUresult::CUDA_SUCCESS);
+        println!("Device available: {:?}", candle_core::utils::cuda_is_available());
     }
 }
